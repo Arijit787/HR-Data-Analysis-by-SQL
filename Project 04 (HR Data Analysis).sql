e238bd39d77e7b88a3c944d392b26bafc0dd57db
Arijit787 <108357674+Arijit787@users.noreply.github.com>
2022-09-07 20:09:48 +0600

    Add files via upload

---
+++ Project 04 (HR Data Analysis).sql
@@ -0,0 +1,132 @@
+SELECT * FROM hr_analytics.hr_data_2;
+
+SELECT AVG(Salary)
+FROM hr_analytics.hr_data_2;
+
+SELECT count(HispanicLatino)
+FROM hr_analytics.hr_data_2;
+
+SELECT MAX(TermReason)
+FROM hr_analytics.hr_data_2;
+
+SELECT count(Department)
+FROM hr_analytics.hr_data_2;
+
+SELECT MAX(RecruitmentSource)
+FROM hr_analytics.hr_data_2;
+
+Select Absences
+from hr_analytics.hr_data_2;
+
+
+SELECT MAX(education)
+FROM hr_analytics.hr_data_3;
+
+SELECT avg(age)
+FROM hr_analytics.hr_data_3;
+
+Select no_oFemale_trainings
+from hr_analytics.hr_data_3;
+
+SELECT MAX(avg_training_score)
+FROM hr_analytics.hr_data_3;
+
+Select count(gender) as Male
+FROM hr_analytics.hr_data_3;
+
+SELECT MAX(satisfaction_level)
+FROM hr_analytics.satisfaction_level;
+
+SELECT * FROM hr_analytics.hr_data_2;
+
+SELECT AVG(Salary)
+FROM hr_analytics.hr_data_2;
+
+SELECT count(HispanicLatino)
+FROM hr_analytics.hr_data_2;
+
+SELECT MAX(TermReason)
+FROM hr_analytics.hr_data_2;
+
+SELECT count(Department)
+FROM hr_analytics.hr_data_2;
+
+SELECT MAX(RecruitmentSource)
+FROM hr_analytics.hr_data_2;
+
+Select Absences
+from hr_analytics.hr_data_2;
+
+
+SELECT MAX(education)
+FROM hr_analytics.hr_data_3;
+
+SELECT avg(age)
+FROM hr_analytics.hr_data_3;
+
+Select no_oFemale_trainings
+from hr_analytics.hr_data_3;
+
+SELECT MAX(avg_training_score)
+FROM hr_analytics.hr_data_3;
+
+Select count(gender) as Male
+FROM hr_analytics.hr_data_3;
+
+SELECT MAX(satisfaction_level)
+FROM hr_analytics.satisfaction_level;
+
+
+SELECT * FROM hr_analytics.hr_data_2;
+
+SELECT AVG(Salary)
+FROM hr_analytics.hr_data_2;
+
+SELECT count(HispanicLatino)
+FROM hr_analytics.hr_data_2;
+
+SELECT MAX(TermReason)
+FROM hr_analytics.hr_data_2;
+
+SELECT count(Department)
+FROM hr_analytics.hr_data_2;
+
+SELECT MAX(RecruitmentSource)
+FROM hr_analytics.hr_data_2;
+
+Select Absences
+from hr_analytics.hr_data_2;
+
+
+SELECT MAX(education)
+FROM hr_analytics.hr_data_3;
+
+SELECT avg(age)
+FROM hr_analytics.hr_data_3;
+
+Select no_oFemale_trainings
+from hr_analytics.hr_data_3;
+
+SELECT MAX(avg_training_score)
+FROM hr_analytics.hr_data_3;
+
+Select count(gender) as Male
+FROM hr_analytics.hr_data_3;
+
+SELECT MAX(satisfaction_level)
+FROM hr_analytics.satisfaction_level;
+
+SELECT avg(number_project)
+FROM hr_analytics.satisfaction_level;
+
+SELECT sum(Work_accident)
+FROM hr_analytics.satisfaction_level;
+
+SELECT sum(promotion_last_5years)
+FROM hr_analytics.satisfaction_level;
+
+SELECT avg(salary)  as Salary_Satisfaction
+FROM hr_analytics.satisfaction_level;
+
+SELECT max(sales)
+FROM hr_analytics.satisfaction_level
